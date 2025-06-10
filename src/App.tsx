865ea39320f8c87153585f29007f845fb91cbfb1
Shane Misra <shane.misra@gmail.com>
2025-06-10 12:22:32 -0600

    add type to Square in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-export default function Square() {
+import {FC, ReactNode} from "react"
+
+const Square : FC = ( ): ReactNode | Promise<ReactNode> => {
   return 
   <>
   <button className="square">X</button>
-  <button className="square">X</button>
   </>;
 }
+
+// Helpful code from Kayla to take permissions for CRA and clean the cache : 
+// 
+// sudo chown -R $(whoami) ~/.npm
+//
+// npm cache clean --force
+export default Square;
